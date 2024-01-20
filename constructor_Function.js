eb62b9a75cbc9cbbba161d970428a9c3a3416fbe
murtaza bazaz <93817703+murtazabazaz@users.noreply.github.com>
2024-01-20 23:28:41 +0530

    Add files via upload

--- constructor_Function.js
+++ constructor_Function.js
@@ -6,8 +6,13 @@ function Employee(name, age, hasPermit, languages){
     this.languages = languages;
 
     this.startWork = function(){
-        console.log(name+" started working now.")
+        console.log(name+" started working now. He knows " + languages+".")
     }
 }
-var employee1 = new Employee("murtaza", 16, true, ["english", "urdu", "hindi"]);
+var employee1 = new Employee("murtaza", 16, true, ["english", "urdu", "hindi"]); 
+var employee2 = new Employee("muied", 16, true, ["english", "urdu"]); 
+var employee3 = new Employee("kaleem", 13, false, ["english", "hindi"]); 
 employee1.startWork();
+employee2.startWork();
+employee3.startWork();
+
