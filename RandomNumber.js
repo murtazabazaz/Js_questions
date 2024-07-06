eea218ec60161d73d9c0a76b7328bb6b6137a46f
murtaz <murtazasbazaz@gmail.com>
2024-07-06 09:26:44 +0530

    new

--- RandomNumber.js
+++ RandomNumber.js
@@ -1,26 +1,26 @@
 const prompt = require('prompt-sync')();
 //Generating a random dice number..
 
-// var a = Math.random();
-// a = Math.floor(a*6)+1;
-// console.log(a)
+ var a = Math.random();
+ a = Math.floor(a*6)+1;
+ console.log(a)
 
 
 // // Love Calculator...
 
-// var male = prompt('Enter your name -: ');
-// var female = prompt('Enter her name -: ');
+var male = prompt('Enter your name -: ');
+ var female = prompt('Enter her name -: ');
 
-// var loveScore = Math.random();
-// loveScore = Math.floor(loveScore*100)+1;
-// console.log('\nThe ❤️  between', male, '&', female, '= ', loveScore,'%.');
+ var loveScore = Math.random();
+ loveScore = Math.floor(loveScore*100)+1;
+ console.log('\nThe ❤️  between', male, '&', female, '= ', loveScore,'%.');
 
-// if(loveScore>60){
-//     console.log('You guys are meant to be...')
-// }
-// else{
-//     console.log('Move on dear!!!', loveScore,'% , is pretty low.')
-// }
+ if(loveScore>60){
+     console.log('You guys are meant to be...')
+ }
+ else{
+     console.log('Move on dear!!!', loveScore,'% , is pretty low.')
+ }
 
 // practice:
 
